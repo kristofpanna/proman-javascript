b134cc5e2384df0489b57b4eea730d764ee0fc75
Panna Kristóf <kristofpanna@gmail.com>
2019-07-25 14:31:59 +0200

    Simplify comparison

--- data_handler.py
+++ data_handler.py
@@ -22,7 +22,7 @@ def get_cards_for_board(board_id):
     all_cards = persistence.get_cards()
     matching_cards = []
     for card in all_cards:
-        if str(card['board_id']) == str(board_id):
+        if card['board_id'] == board_id:
             card['status'] = get_card_status(card['status_id'])  # Set textual status for the card
             matching_cards.append(card)
     return matching_cards
